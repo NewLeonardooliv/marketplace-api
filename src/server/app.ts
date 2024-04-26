275a162f88d7017b96afbd7088cb8c906955666a
Luciano Martins <luciano.martinsjr@hotmail.com>
2024-04-26 19:24:40 -0300

    queue

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import { enviarMensagem } from 'src/services/Amqp/AmqpService';
 
 const app = express();
+
+require('dotenv/config');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// enviarMensagem('testes', 'Leonardo').catch(console.error);
+
+ 
 
 export { app };
